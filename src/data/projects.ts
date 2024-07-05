29c2caa62654f2ab55d8b0842fc47ec62a8d1dab
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2024-07-05 15:22:03 -0500

    Update Project List

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,30 +5,20 @@ const projects = [
     link: 'https://github.com/magnifydev/magnify',
   },
   {
-    name: 'Scripty',
-    description: 'Envision a bot.',
-    link: 'https://github.com/scriptydev/prescripty',
-  },
-  {
-    name: 'LMRA',
-    description: 'Lawn mower rights matter. Period.',
-    link: 'https://github.com/lmraorg/LMRA',
-  },
-  {
     name: 'Plane',
     description:
       'A simple and dynamic asynchronous Python wrapper for the Ravy API.',
     link: 'https://github.com/GoogleGenius/plane',
   },
   {
-    name: 'Someone',
-    description: 'Bringing @someone back to Discord.',
-    link: 'https://github.com/GoogleGenius/someone',
+    name: 'Scripty',
+    description: 'Envision a bot.',
+    link: 'https://github.com/scriptydev/prescripty',
   },
   {
-    name: 'Deputy Warden',
-    description: 'A fun prison bot for your Discord server.',
-    link: 'https://github.com/GoogleGenius/deputy-warden',
+    name: '@Someone',
+    description: 'Bringing @someone back to Discord.',
+    link: 'https://github.com/GoogleGenius/someone',
   },
 ];
 
